74cf4f20dc170413f6e6df2cb4eacc3f5562235b
Megan Lo <hkmeganlo@gmail.com>
2020-08-21 17:17:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -85,14 +85,14 @@ def game_hash
         
         {
           :player_name => "DeSagna Diop",
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
-          :slam_dunks => 2
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         }, 
   
       ]
