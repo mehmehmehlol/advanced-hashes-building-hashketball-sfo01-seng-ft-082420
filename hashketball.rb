4a5c2dd05cca04d42edce22b28d0a4d7087cfecf
Megan Lo <hkmeganlo@gmail.com>
2020-08-21 17:21:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,11 +46,11 @@ def game_hash
           :number => 1,
           :shoe => 19,
           :points => 26,
-          :rebounds => 19,
-          :assists => 10,
+          :rebounds => 11,
+          :assists => 6,
           :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          :blocks => 8,
+          :slam_dunks => 5
         },
         
         {
