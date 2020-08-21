18f22f039702802f9cc953d9bb049be3c2b22762
Megan Lo <hkmeganlo@gmail.com>
2020-08-21 17:16:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,20 +56,20 @@ def game_hash
     },
     
     :away => {
-      :team_name => ,
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => ["turquoise", "purple"],
       :players => [
         {
-          :player_name => ,
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
-        }
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+        }, 
   
       ]
     }
